667d27b8decfb7fdbf38d324c5b6710b67c14099
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2024-10-28 17:08:43 +0200

    Refactor type predicate in 12-type-predicates-with-filter.problem.ts

--- src/12-type-predicates-with-filter.problem.ts
+++ src/12-type-predicates-with-filter.problem.ts
@@ -3,9 +3,7 @@ import { Equal, Expect } from "../helpers/type-utils.ts";
 
 export const values = ["a", "b", undefined, "c", undefined];
 
-const isString = (value: string | undefined): value is string => Boolean(value);
-
-const filteredValues = values.filter((value) => value !== undefined);
+const filteredValues = values.filter((value): value is string => Boolean(value));
 
 it("Should filter out the undefined values", () => {
   expect(filteredValues).toEqual(["a", "b", "c"]);
